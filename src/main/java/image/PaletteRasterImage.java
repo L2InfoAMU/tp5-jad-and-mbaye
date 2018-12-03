a20aec8721fedbd4321fd5cc27ca8ba6c3870dc2
m17030283 <ahmadoubambambaye2@gmail.com>
2018-12-03 17:04:22 +0100

    premiere partie

---
+++ src/main/java/image/PaletteRasterImage.java
@@ -0,0 +1,63 @@
+package image;
+
+import javafx.scene.paint.Color;
+
+import java.util.List;
+
+public class PaletteRasterImage implements Image {
+    int width;
+    int height;
+    int [][] indexOfColors;
+    List<Color> palette;
+
+    public PaletteRasterImage(int width, int height, int[][] indexOfColors, List<Color> palette) {
+        this.width = width;
+        this.height = height;
+        this.indexOfColors = indexOfColors;
+        this.palette = palette;
+    }
+
+    public PaletteRasterImage(Color[][] pixels) {
+
+
+    }
+
+    public void createRepresentation() {
+
+    }
+
+    public void setPixelColor(Color color, int x, int y) {
+
+    }
+
+    @Override
+    public Color getPixelColor(int x, int y) {
+        return null;
+    }
+
+    public void setPixelsColor(Color[][] pixels) {
+
+    }
+
+    private void setPixelsColor(Color color) {
+
+    }
+
+    @Override
+    public int getWidth() {
+        return 0;
+    }
+
+    @Override
+    public int getHeight() {
+        return 0;
+    }
+
+    protected void setWidth(int width) {
+        this.width = width;
+    }
+
+    protected void setHeight(int height) {
+        this.height = height;
+    }
+}
